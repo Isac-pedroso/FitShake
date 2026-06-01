aa296bdd396f6ce521bb5a6793b5f8a13e3572b0
Isacao lindao <isacsouzapedroso@gmail.com>
2026-06-01 20:16:20 -0300

    Estrutura back

---
+++ FitShake/fitshake_back_end/src/test/java/com/app/fitshake/FitshakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fitshake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitshakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
